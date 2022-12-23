1a69e3e622d09755d1baf727c84d323da76eeaaa
Martin <45198686+ridlees@users.noreply.github.com>
2022-12-23 13:50:19 +0100

    Adds jingle through printf '\7'

--- 202020.sh
+++ 202020.sh
@@ -6,8 +6,10 @@ timer=1200
 while true
 do
     sleep 1
+    printf '\7'
     notify-send "20/20/20" "Look at something in 20 meter distance"
     sleep 20
+    printf '\7'
     notify-send "DONE" "You can look back at your screen"
     sleep $timer
 done
